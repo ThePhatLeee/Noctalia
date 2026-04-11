5ef3f5a309330f4c312396dd9e00ed8691a081b7
Phát Lê <phat.le@thephatle.dev>
2026-04-11 06:27:52 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,32 +10,7 @@ dnf5 -y copr enable lionheartp/Hyprland
 # noctalia-hyprland-meta: The entire Wayland/Hyprland core ecosystem
 # kitty: Your lifeline to install Nix tomorrow
 RUN dnf5 install -y \
-    hyprland \
-    noctalia-hyprland-meta \
-    noctalia-shell \
-    noctalia-qs \
-    matugen \
-    uwsm \
-    xdg-desktop-portal-hyprland \
-    hyprcursor \
-    hyprpolkitagent \
-    hypridle \
-    hyprlock \
-    hyprpaper \
-    cliphist \
-    hyprqt6engine \
-    hyprland-qt-support \
-    qt6ct \
-    nwg-look \
-    waypaper \
-    hyprlauncher \
-    hyprshot \
-    hyprpicker \
-    hyprsunset \
-    hyprsysteminfo \
-    hyprpwcenter \
-    hyprshutdown \
-    gpu-screen-recorder
+     noctalia-hyprland-meta \
 ### 3. Disable the COPR
 dnf5 -y copr disable lionheartp/Hyprland
 
